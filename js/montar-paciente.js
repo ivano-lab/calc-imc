916158c52f12357144b8b615f72325ad147cda1c
Ívano Fontes <ivanofnts@gmail.com>
2022-09-14 23:25:35 -0300

    adicionado fluxo condicional saliente

--- js/montar-paciente.js
+++ js/montar-paciente.js
@@ -12,25 +12,28 @@ function montarPaciente() {
         pegarImc        : function() {
                             if(this.altura != 0) {
                                 var imc = this.peso / (this.altura * this.altura);
+                                imc = imc.toFixed(1);
                                 var situacao;
                                 if (imc < 17) {
                                     situacao = "Muito abaixo do peso";
+                                } else if (imc >= 17 && imc < 18.5) {
+                                    situacao = "Abaixo do peso";
+                                } else if (imc >= 18.5 && imc < 25) {
+                                    situacao = "Peso ideal";
+                                } else if (imc >= 25 && imc < 30) {
+                                    situacao = "Sobrepeso";
+                                } else if (imc >= 30 && imc < 35) {
+                                    situacao = "Obesidade";
+                                } else if (imc >= 35 && imc < 40) {
+                                    situacao = "Obesidade Severa";
                                 } else {
-                                    situacao = "Valor padrao";
+                                    situacao = "Obesidade Morbida"; 
                                 }
                                 return [imc, situacao];
                             } else {
                                 alert("A altura deve ser maior que zero!"); 
                             }
-                        }/*,
-        pegarSituacao   : function() {
-                            if(this.altura > 0) {
-                                var situacao = "Muito abaixo do peso";
-                                return situacao;
-                            } else {
-                                var situacao = "Padrao";
-                            }
-                        }*/
+                        }
                     }
                 
                 return pacienteAtual;
